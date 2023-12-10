609f9493b4f3a89344193d07ef1457a60f0c0601
Heesu Jang <heesu52@naver.com>
2023-12-10 16:35:11 +0900

    최종 이미지를 보내는것으로 수정

--- backend-server/EmotiArt_app/views.py
+++ backend-server/EmotiArt_app/views.py
@@ -1,48 +1,34 @@
-from django.http import JsonResponse
-from django.http import FileResponse
-import io
-from PIL import Image
+from django.http import JsonResponse, HttpResponseNotFound, FileResponse
 from django.views.decorators.csrf import csrf_exempt
+import os
 
 @csrf_exempt
 def receive_image(request):
-    if request.method == 'POST' and request.FILES['drawing']:
+    if request.method == 'POST' and request.FILES.get('drawing'):
         image_file = request.FILES['drawing']
 
-        # 이미지 처리 로직
-        image = Image.open(image_file)
-        # 예시로 이미지를 회전시켜봅시다
-        rotated_image = image.rotate(90)
+        # 이미지 저장 경로
+        save_path = 'C:/Users/JangHeesu/PycharmProjects/backend/backend-server/frontend_save_image/'
 
-        # 임시 파일로 이미지를 저장
-        buffer = io.BytesIO()
-        rotated_image.save(buffer, format='PNG')
-        buffer.seek(0)
+        # 클라이언트로부터 받은 이미지를 저장
+        with open(os.path.join(save_path, image_file.name), 'wb+') as destination:
+            for chunk in image_file.chunks():
+                destination.write(chunk)
 
-        # 임시 파일을 응답으로 전송
-        return FileResponse(buffer, content_type='image/png')
+        # 저장한 이미지를 읽어와서 반환
+        image_data_list = []
+        final_image_path = 'C:/Users/JangHeesu/PycharmProjects/backend/final_emotiart_image/'
+        for filename in os.listdir(final_image_path):
+            if os.path.isfile(os.path.join(final_image_path, filename)):
+                with open(os.path.join(final_image_path, filename), 'rb') as file:
+                    image_data = file.read()
+                    image_data_list.append(image_data)
+
+        if image_data_list:
+            # 이미지 데이터를 합쳐서 반환
+            combined_image_data = b''.join(image_data_list)
+            return FileResponse(combined_image_data, content_type='image/png')
+        else:
+            return HttpResponseNotFound('No images found in the final_emotiart_image directory.')
     else:
-        return JsonResponse({'error': 'No image found in the request.'}, status=400)
-
-'''
-def receive_image(request):
-    if request.method == 'POST' and request.FILES['drawing']:
-        image_file = request.FILES['drawing']
-
-        # 이미지를 열어서 PIL 이미지 객체로 변환
-        image = Image.open(image_file)
-
-        # 이미지를 회전시킴 (예시로 90도 회전)
-        rotated_image = image.rotate(90)
-
-        # 회전한 이미지를 PNG 형식으로 변환
-        buffer = rotated_image.convert("RGB")
-        buffer.save("rotated_image.png", format='PNG')
-        buffer.seek(0)
-
-        # 회전된 이미지를 클라이언트에게 전송
-        return FileResponse(buffer, content_type='image/png')
-    else:
-        return JsonResponse({'error': 'No file named "drawing" found in the request.'}, status=400)
-    '''
-
+        return JsonResponse({'error': 'No image found in the request or invalid request method.'}, status=400)
